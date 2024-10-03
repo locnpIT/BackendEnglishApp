1f5292c916273dfe55acafd4e031495d88666791
locnpIT <npl2004qn@gmail.com>
2024-10-04 00:23:24 +0700

    upload code

---
+++ backendenglishapp/src/main/java/com/example/project/Models/FlashCard.java
@@ -0,0 +1,48 @@
+package com.example.project.Models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "flashcard")
+public class FlashCard {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String front;
+	
+	private String back;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFront() {
+		return front;
+	}
+
+	public void setFront(String front) {
+		this.front = front;
+	}
+
+	public String getBack() {
+		return back;
+	}
+
+	public void setBack(String back) {
+		this.back = back;
+	}
+	
+	
+	
+	
+}
